3d70de1d306ae08c88014f35e653892bfd4b780a
muzaffarovhayotbek <hayotbek0511@gmail.com>
2025-02-06 01:58:52 +0500

    end home animation

--- src/pages/Error/ErrorPage.jsx
+++ src/pages/Error/ErrorPage.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import './ErrorPage.css'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import me from '../../assets/hayotbek.jpg';
+import './ErrorPage.css';
 
 function ErrorPage() {
   return (
     <div>
-       <div className="error-container container">
-      <h2>404</h2>
-      <p>Bunday sahifa mavjud emas.</p>
-      <NavLink to='/'>Bosh sahifaga qaytish</NavLink>
+      <div className="container mx-auto px-5 max-w-screen-sm flex flex-col-reverse items-center gap-5 sm:flex-row justify-between error-container">
+        <div className="error-container">
+          <h2>404</h2>
+          <p>Bunday sahifa mavjud emas</p>
+          <a href="/" className="">
+            Bosh sahifaga qaytish
+          </a>
+        </div>
+        <img src={me} alt="" />
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default ErrorPage
+export default ErrorPage;
